7da3f893e6f79bd3762b20f5ea648a1ac576dc6e
x3lay <x3layka@gmail.com>
2026-06-15 20:43:57 +0300

    Update config

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8785233686:AAFe77G_0jSc4m8Gf2mTR2ANRg95phS_9Pk")
 ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "8229778449"))
-WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://YOUR_DOMAIN_HERE")
+WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://ipoint.onrender.com")
