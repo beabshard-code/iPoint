e414de3e88246b054c64256aca734faad97e5042
x3lay <x3layka@gmail.com>
2026-06-15 20:40:28 +0300

    Update bot token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8785233686:AAFbP3TWGLuQ6e6pyFIxoFye_6ui-4HG67M")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8785233686:AAFe77G_0jSc4m8Gf2mTR2ANRg95phS_9Pk")
 ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "8229778449"))
 WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://YOUR_DOMAIN_HERE")
